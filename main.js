38bb69a7e5375a97ec28f82cd6b05ce1b0499bea
Sasa0368 <98093637+Sasa0368@users.noreply.github.com>
2023-04-03 14:45:49 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://client.pylexnodes.net/earn
+// @match        https://client.pylexnodes.net/earn?err=none
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
+
+generate()
